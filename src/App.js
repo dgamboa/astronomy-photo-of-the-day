1bd032b096ef7ee7a29859c1a7d70728a3df0122
Daniel Gamboa <danielgamboa10@gmail.com>
2021-01-20 17:17:56 -0600

    Update readme

--- src/App.js
+++ src/App.js
@@ -2,20 +2,70 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios'
 import { BASE_URL } from './constants/base_url'
 import { API_KEY } from './constants/api_key'
+import Apod from './components/Apod'
 import "./App.css";
 
 function App() {
-  const [date, setDate] = useState();
-  const [img, setImg] = useState();
-  const [explanation, setExplanation] = useState();
+  const today = new Date();
+  const initialDate = today.toISOString().split('T')[0];
 
+  const [title, setTitle] = useState('');
+  const [date, setDate] = useState(initialDate);
+  const [img, setImg] = useState('');
+  const [imgAlt, setImgAlt] = useState('');
+  const [explanation, setExplanation] = useState('');
 
+  useEffect(() => {
+    const fetchApod = () => {
+      axios.get(`${BASE_URL}?api_key=${API_KEY}&date=${date}`)
+        .then(res => {
+          console.log(res);
+          setTitle(res.data.title);
+          setDate(res.data.date);
+          setImg(res.data.url);
+          setImgAlt(res.data.title);
+          setExplanation(res.data.explanation);
+        })
+        .catch(err => {
+          console.log(err);
+          debugger
+        })
+    }
+    fetchApod();
+  }, [date])
+
+  const prevDate = () => {
+    let dateObj = new Date(date);
+    dateObj.setDate(dateObj.getDate() - 1);
+    const newDate = dateObj.toISOString().split('T')[0];
+    console.log(newDate);
+    setDate(newDate);
+  }
+
+  const nextDate = (e) => {
+    if (date === initialDate) {
+      e.preventDefault();
+      console.log("Already showing the latest picture!")
+    } else {  
+      let dateObj = new Date(date);
+      dateObj.setDate(dateObj.getDate() + 1);
+      const newDate = dateObj.toISOString().split('T')[0];
+      console.log(newDate);
+      setDate(newDate);
+    }
+  }
 
   return (
     <div className="App">
-      <button onClick={() => console.log("Clicked")}>
-        Get Today's Picture
-      </button>
+      <button className="prev" onClick={prevDate}>&#9668; Prev</button>
+      <Apod
+        title={title}
+        date={date}
+        img={img}
+        imgAlt={imgAlt}
+        explanation={explanation}
+      />
+      <button className="next" onClick={nextDate}>Next &#9658;</button>
     </div>
   );
 }
