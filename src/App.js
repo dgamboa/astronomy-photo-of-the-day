228d37995fb910ff483cb864e3ac0a2526f7ae51
Daniel Gamboa <danielgamboa10@gmail.com>
2021-01-20 15:03:58 -0600

    Add axios to dependencies and add api_key file to gitignore

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
+import axios from 'axios'
+import { BASE_URL } from './constants/base_url'
+import { API_KEY } from './constants/api_key'
 import "./App.css";
 
 function App() {
   const [date, setDate] = useState();
   const [img, setImg] = useState();
   const [explanation, setExplanation] = useState();
+
+
 
   return (
     <div className="App">
---
+++ src/constants/base_url.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.nasa.gov/planetary/apod'
+export const API_KEY = 'IPHaFBHNVrqCIDC6KWZXH7k7kkcfZQ3hI7l5exqY'
