22f67e4b06bcaedb81cdcff548695844416731b9
Daniel Gamboa <danielgamboa10@gmail.com>
2021-01-20 17:16:19 -0600

    Add basic styling, build Apod component and implement next / previous functionality

--- src/components/Apod.js
+++ src/components/Apod.js
@@ -1,1 +1,14 @@
 import React from 'react'
+
+export default function Apod(props) {
+  const { title, date, img, explanation, imgAlt } = props;
+  
+  return (
+    <div className="apod">
+      <h1>{title}</h1>
+      <h3>{date}</h3>
+      <img src={img} alt={imgAlt} />
+      <p>{explanation}</p>
+    </div>
+  )
+}
