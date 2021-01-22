22a410e9bc6d4aa1c9c2f7c9021d403cabd5e0d0
Daniel Gamboa <danielgamboa10@gmail.com>
2021-01-21 22:35:21 -0600

    Implement prev and next buttons including styling

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
           debugger
         })
     }
-    // fetchImage();
+    fetchImage();
   }, [date])
 
   const prevDate = () => {
@@ -64,17 +64,19 @@ function App() {
   return (
     <div className="App">
       <Nav />
-      {/* <button className="prev" onClick={prevDate}>&#9668; Prev</button> */}
+      
       <Image
         title={title}
         date={date}
         img={img}
         explanation={explanation}
         imgLink={imgLink}
+        prevDate={prevDate}
+        nextDate={nextDate}
       />
-      {/* <button className="next" onClick={nextDate}>Next &#9658;</button> */}
+      
       <Footer />
-      <a href="https://github.com/dgamboa/nasa-photo-of-the-day/tree/daniel-gamboa"><img loading="lazy" width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_gray_6d6d6d.png?resize=149%2C149" class="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1" /></a>
+      <a href="https://github.com/dgamboa/nasa-photo-of-the-day/tree/daniel-gamboa"><img loading="lazy" width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_gray_6d6d6d.png?resize=149%2C149" className="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1" /></a>
     </div>
   );
 }
--- src/components/Image.js
+++ src/components/Image.js
@@ -5,8 +5,10 @@ import Description from './Description'
 const ImageSection = styled.section`
   /* border: solid 1px black; Delete */
   border-top: 1px solid rgba(255,255,255,0.08);
+  border-bottom: 1px solid rgba(0,0,0,0.5);
   margin: 0 auto;
-  width: 75%;
+  padding-bottom: 5rem;
+  width: 80%;
   max-width: 80rem;
 
   .image-header {
@@ -23,16 +25,45 @@ const ImageSection = styled.section`
     }
   }
 
+  .image {
+    position: relative;
+  }
+
+  .prev {
+    position: absolute;
+    padding: 5rem;
+    border: none;
+    cursor: pointer;
+    height: 99.5%;
+    opacity: 0;
+    &:hover {
+      opacity: 0.2;
+    }
+  }
+
+  .next {
+    position: absolute;
+    right: 0;
+    padding: 5rem;
+    border: none;
+    cursor: pointer;
+    height: 99.5%;
+    opacity: 0;
+    &:hover {
+      opacity: 0.2;
+    }
+  }
+
   img {
-    max-height: 100%;
-    max-width: 100%;
     border-radius: 4px;
     box-shadow: 0 0 10px #222;
+    max-width: 100%;
+    max-height: 100%;
   }
 `;
 
 export default function Image(props) {
-  const { title, date, img, explanation, imgLink } = props;
+  const { title, date, img, explanation, imgLink, prevDate, nextDate } = props;
   
   const [collapsedDesc, setCollapsedDesc] = useState(true);
 
@@ -47,11 +78,15 @@ export default function Image(props) {
         </h3>
         <h3 className="date">{date}</h3>
       </div>
+      
       <Description explanation={explanation} collapsedDesc={collapsedDesc}/>
+      
       <div className="image">
+        <button className="prev" onClick={prevDate}>&#9668; Prev</button>
         <a href={imgLink}>
           <img src={img} alt={title}/>
         </a>
+        <button className="next" onClick={nextDate}>Next &#9658;</button>
       </div>
     </ImageSection>
   )
